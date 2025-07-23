fcb70cbc2ac996a5830d02934bf6a27befe60f75
SaiSrichandra <53914157+SaiSrichandra@users.noreply.github.com>
2025-07-23 13:56:57 -0400

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://ai-summarizer-k029.onrender.com/api";
+const API_BASE = "https://ai-summarizer-3j8a.onrender.com";
 
 function saveToken(token) {
   localStorage.setItem("token", token);
